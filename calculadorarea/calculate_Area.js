89c7e8b549979fd877ebefa0ff8e9d4e3c0efa8a
ijovkoo <joaquin.pizarro11@inacapmail.cl>
2026-06-03 13:44:36 -0400

    Terminado el ejercicio de calcular area

---
+++ calculadorarea/calculate_Area.js
@@ -0,0 +1,8 @@
+let lenght;
+let width;
+function calculaterarea(){
+    lenght= parseFloat(Document.getElementById("lenght").value);
+    width = parseFloat(document.getElementById('width').value);
+    let area = lenght*width;
+}
+
